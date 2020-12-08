d97263fdaea53a352d586b03b0d933858609a770
CristianJ <cgjativa@udlanet.ec>
2020-12-08 10:19:46 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { CaracteristicasComponent } from './caracteristicas/caracteristicas.component';
+import { OpcionesComponent } from './opciones/opciones.component';
+import { BeneficiosComponent } from './beneficios/beneficios.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ServiciosComponent,
+    CaracteristicasComponent,
+    OpcionesComponent,
+    BeneficiosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
